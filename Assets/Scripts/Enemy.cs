94441195f0b6e04347f602af51648079eeff99fa
Germetic <nazarvkvk@Mail.ru>
2017-03-13 21:26:15 +0200

    Rabbit , and new animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,18 +8,39 @@ public class Enemy : MonoBehaviour
 
 
     [SerializeField]
-    GameObject Player;
+    private GameObject Player;
+	Animator ani;
 
     private void Start()
-    {
+	{	ani = GetComponent<Animator> ();
         Player = GameObject.FindGameObjectWithTag("Player");
+		ani.SetBool("ImFollow",false);
     }
 
     void OnTriggerStay2D(Collider2D col)
     {
         if (col.gameObject.tag == "fog")
-        {
+        {	
+			if (transform.position.x > Player.transform.position.x) {
+				transform.rotation = Quaternion.Euler (0, 0, 0);
+			} else {
+				transform.rotation = Quaternion.Euler(0, 180, 0);
+			}
             transform.position = Vector3.Lerp(transform.position, Player.transform.position, 0.01f);
         }
     }
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "fog")
+		{
+			ani.SetBool("ImFollow",true);
+		}
+	}
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "fog")
+		{
+			ani.SetBool("ImFollow",false);
+		}
+	}
 }
